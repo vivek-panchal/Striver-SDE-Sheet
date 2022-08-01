04e17f6c76e6a65ea7e8a8a5b559349c29ce4970
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-01 19:26:01 +0530

    Create Merge two sorted array.cpp

---
+++ Merge two sorted array.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	int j=0;
+        for(int i=m; i<m+n; i++){
+            arr1[i]=arr2[j++];  
+        }
+          sort(arr1.begin() , arr1.end());  
+    return arr1;
+}
