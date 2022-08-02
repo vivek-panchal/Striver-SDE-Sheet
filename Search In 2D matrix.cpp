27b7693340c65e5255612016e8b5ef0a3590bf97
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-02 18:13:58 +0530

    Create Search In 2D matrix.cpp

---
+++ Search In 2D matrix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h> 
+#include <vector>
+
+bool findTargetInMatrix(vector < vector < int >> & mat, int m, int n, int target) {
+    // Write your code here.
+    int row = mat.size();
+        int col = mat[0].size();
+        
+        int start = 0;
+        int end  = row*col-1;
+        
+        int mid = start + (end-start)/2;
+        
+        while(start<=end) {
+            
+            int element = mat[mid/col][mid%col];
+            
+            if(element == target) {
+                return 1;
+            }
+            
+            if(element < target) {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+             mid = start + (end-start)/2;    
+        }
+        return 0;
+}
