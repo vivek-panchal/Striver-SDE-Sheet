c64937ba230729610b8d8f129a55ba008b0460ed
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-03 18:22:03 +0530

    Update Longest Substring Without Repeating Characters.cpp

--- Longest Substring Without Repeating Characters.cpp
+++ Longest Substring Without Repeating Characters.cpp
@@ -8,9 +8,12 @@ int uniqueSubstrings(string s)
         int n= s.size();
         int length=0;
         while(right<n){
-            if(mpp[s[right]]!=-1)
+            // check right is present into set or not
+            if(mpp[s[right]]!=-1)// right present at set , so now we chect jha abhi left hai or jis index per right set me present hai unme se konsa index bda hai jo bhi bda hai left ko vha put kar do
                 left=max(mpp[s[right]]+1 , left);
+            // set me index update kar do repeating character ka , abhi vo right index per hai
             mpp[s[right]]=right;
+            // max length store kar lo
             length=max(length,right - left +1);
             right++;
         }
