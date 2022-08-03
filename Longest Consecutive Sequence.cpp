dc2ad24140d9b576bd5e178ee05bf51e518c7b99
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-03 15:22:56 +0530

    Update Longest Consecutive Sequence.cpp

--- Longest Consecutive Sequence.cpp
+++ Longest Consecutive Sequence.cpp
@@ -1,3 +1,37 @@
+//******************************************   OPTIMAL APPROACH   *********************************************************************************************
+#include <bits/stdc++.h> 
+int lengthOfLongestConsecutiveSequence(vector<int> &nums, int n) {
+    
+    unordered_set<int> hashset;
+        
+        if(nums.size()==0){//base cases 
+            return 0;
+        }
+        if(nums.size()==1){
+            return 1;
+        }
+
+        for(int num: nums){
+            hashset.insert(num);
+        }
+        
+        int longestStreak=0;
+        
+        for(int num: nums){
+            if(hashset.find(num-1) == hashset.end()){
+                int currNum=num;
+                int currStreak=1;
+                
+                while(hashset.find(currNum+1)!= hashset.end()){
+                    currNum+=1;
+                    currStreak+=1;
+                }
+                longestStreak=max(longestStreak,currStreak);
+            }
+        }
+        return longestStreak;
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
 #include <bits/stdc++.h> 
 int lengthOfLongestConsecutiveSequence(vector<int> &nums, int n) {
     // Write your code here.
