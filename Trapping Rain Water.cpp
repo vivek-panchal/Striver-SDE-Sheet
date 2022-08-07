91edec50916a83dc32f6734476fb0a4ada523ba9
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-07 14:45:37 +0530

    Create Trapping Rain Water.cpp

---
+++ Trapping Rain Water.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+long getTrappedWater(long *height, int n){
+    // Write your code here
+        long left=0; long right=n-1;
+        long res=0;
+        long maxleft=0, maxright=0;
+        
+        while(left<=right){
+            
+            if(height[left]<=height[right]){
+
+                if(height[left]>=maxleft) maxleft=height[left];
+                else res+=maxleft-height[left];
+                
+                left++;
+            }
+            else{
+
+                if(height[right]>=maxright) maxright= height[right];
+                else res+=maxright-height[right];
+                
+                right--;
+            }
+        }
+        return res;
+}
